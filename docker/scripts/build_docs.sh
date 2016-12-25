2d8fbb4a6f76dab488a1d03c693d4afff90970a9
Victor Fernandez <victor.j.fdez@gmail.com>
2016-12-25 01:56:28 -0700

    some updates

--- docker/scripts/build_docs.sh
+++ docker/scripts/build_docs.sh
@@ -4,16 +4,22 @@ source './docker/scripts/common_container_utils.sh'
 
 set -x
 if is_env_develop ; then
+  branch="`git branch | grep \* | cut -d ' ' -f2`"
+  echo "currently on '$branch'"
   git config user.name "Victor Fernandez"
   git config user.email "victor.j.fdez@gmail.com"
-  git checkout -B "gh-pages"
+  git checkout --orphan -B "gh-pages"
   # if environment is develop/development then htmls docs for this
   # project
   docker run -v `pwd`:/usr/src/app `gen_image_name` run prod:docs 
+  rm -rf `find . -depth 1 | grep -v 'index.html\|assets\|.git'`
   # deploy to github pages
   git status
   git add ./assets/ ./index.html
   git commit -m "documentation for `package_version`"
   git push origin gh-pages
+  # return to original branch
+  git checkout "$branch"
+  git reset --hard
 fi
 set +x
