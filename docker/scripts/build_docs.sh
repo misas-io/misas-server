6d48f0a55693b61acd3b63664e3da1de3f42b894
Victor Fernandez <victor.j.fdez@gmail.com>
2016-12-25 01:21:18 -0700

    fix docs

--- docker/scripts/build_docs.sh
+++ docker/scripts/build_docs.sh
@@ -4,13 +4,14 @@ source './docker/scripts/common_container_utils.sh'
 
 set -x
 if is_env_develop ; then
+  git config user.name "Victor Fernandez"
+  git config user.email "victor.j.fdez@gmail.com"
+  git checkout -B "gh-pages"
   # if environment is develop/development then htmls docs for this
   # project
   docker run -v `pwd`:/usr/src/app `gen_image_name` run prod:docs 
   # deploy to github pages
-  git config user.name "Victor Fernandez"
-  git config user.email "victor.j.fdez@gmail.com"
-  git checkout -b "gh-pages"
+  git status
   git add ./assets/ ./index.html
   git commit -m "documentation for `package_version`"
   git push origin gh-pages
